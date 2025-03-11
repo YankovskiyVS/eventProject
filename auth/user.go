c760bebf8aa8db7ca54f65dc7645d302701f170c
YankovskiyVS <vld.s.yan567@gmail.com>
2025-03-11 18:18:06 +0300

    user defined

--- auth/user.go
+++ auth/user.go
@@ -1,7 +1,10 @@
 package main
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Username string `json:"username" bson:"username"`
-	Password string `json:"password" bson:"password"`
-	Role     string `json:"role" bson:"role"`
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Username string             `json:"username" bson:"username"`
+	Password string             `json:"password" bson:"password"`
+	Role     string             `json:"role" bson:"role"`
 }
