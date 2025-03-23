a0b06de234f03af730c43928deefb7216bab99e6
YankovskiyVS <vld.s.yan567@gmail.com>
2025-03-24 02:19:06 +0300

    fixed kafka producer to the pattern

--- events/kafka/producer.go
+++ events/kafka/producer.go
@@ -1,29 +1,50 @@
 package kafka
 
-import "github.com/IBM/sarama"
+import (
+	"github.com/IBM/sarama"
+	transactionaloutbox "github.com/YankovskiyVS/eventProject/events/internal/transactional_outbox"
+)
 
 type KafkaProducer struct {
-	Producer sarama.SyncProducer
+	producer sarama.SyncProducer
 }
 
 func NewKafkaProducer(brokers []string) (*KafkaProducer, error) {
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll // Better durability
 
 	producer, err := sarama.NewSyncProducer(brokers, config)
 	if err != nil {
 		return nil, err
 	}
 
-	return &KafkaProducer{Producer: producer}, nil
+	return &KafkaProducer{
+		producer: producer,
+	}, nil
 }
 
-func (kp *KafkaProducer) SendMessage(topic, message string) error {
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.StringEncoder(message),
+func (kp *KafkaProducer) SendMessage(event transactionaloutbox.Message) error {
+	headers := make([]sarama.RecordHeader, 0, len(event.Headers))
+
+	for k, v := range event.Headers {
+		headers = append(headers, sarama.RecordHeader{
+			Key:   []byte(k),
+			Value: []byte(v),
+		})
 	}
 
-	_, _, err := kp.Producer.SendMessage(msg)
+	msg := &sarama.ProducerMessage{
+		Topic:   event.Topic,
+		Key:     sarama.StringEncoder(event.Key),
+		Value:   sarama.StringEncoder(event.Body),
+		Headers: headers,
+	}
+
+	_, _, err := kp.producer.SendMessage(msg)
 	return err
+}
+
+func (kp *KafkaProducer) Close() error {
+	return kp.producer.Close()
 }
