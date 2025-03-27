5b56a271c1562104948198bb6b41a23367312a8b
YankovskiyVS <vld.s.yan567@gmail.com>
2025-03-28 00:29:37 +0300

    added context to the repo

--- orders/internal/domain/repositories/order_repository.go
+++ orders/internal/domain/repositories/order_repository.go
@@ -1,15 +1,17 @@
 package repositories
 
 import (
+	"context"
+
 	"github.com/YankovskiyVS/eventProject/orders/internal/domain/entities"
 	"github.com/google/uuid"
 )
 
 type OrderRepository interface {
-	Create(order *entities.Order) (entities.Order, error)
-	Update(order *entities.Order) (*entities.Order, error)
-	FindAll(userID int) ([]*entities.Order, error)
-	FindByID(userID int, id uuid.UUID) (*entities.Order, error)
-	GetCurrent(userID int) (*entities.Order, error)
-	PayCurrent(order *entities.Order) (*entities.Order, error)
+	Create(ctx context.Context, order *entities.Order) (entities.Order, error)
+	Update(ctx context.Context, order *entities.Order) (*entities.Order, error)
+	FindAll(ctx context.Context, userID int) ([]*entities.Order, error)
+	FindByID(ctx context.Context, userID int, id uuid.UUID) (*entities.Order, error)
+	GetCurrent(ctx context.Context, userID int) (*entities.Order, error)
+	PayCurrent(ctx context.Context, order *entities.Order) (*entities.Order, error)
 }
